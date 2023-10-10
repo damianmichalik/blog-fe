08775b69f72710737eb6efc2738e404d4f107e66
Damian Michalik <damian.michalik@gmail.com>
2023-10-10 19:38:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+GburTravel - Next.js
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+    return (
+        <main >
+            <div>
+                <h1>GburTravel</h1>
+                <h2>Może nie najtaniej, ale jako tako</h2>
+            </div>
+        </main>
+    )
+}
